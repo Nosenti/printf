2347455d4546c1b9940ad168b08b9236b67fe83b
Innocent Ingabire <inno100os@gmail.com>
2023-03-29 13:57:27 -0700

    new changes

--- _printf.c
+++ _printf.c
@@ -1,64 +1,89 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+int handle_char(va_list arg_list);
+int handle_string(va_list arg_list);
+int handle_percent(void);
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - function to print number of characters
+ * @format: char parameter
+ * Return: character.
  */
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
+	int char_count;
+	va_list arg_list;
 
-if (format == NULL)
-return (-1);
-
-va_start(list, format);
-
-for (i = 0; format && format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
-printed_chars++;
-}
-else
-{
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
-}
-}
-
-print_buffer(buffer, &buff_ind);
-
-va_end(list);
-
-return (printed_chars);
+	char_count = 0;
+	va_start(arg_list, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					char_count += handle_char(arg_list);
+					break;
+				case 's':
+					char_count += handle_string(arg_list);
+					break;
+				case '%':
+					char_count += handle_percent();
+					break;
+				default:
+					putchar('%');
+					putchar(*format);
+					char_count += 2;
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			char_count++;
+		}
+		format++;
+	}
+	va_end(arg_list);
+	return (char_count);
 }
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * handle_char - function that handles character arguments.
+ * @arg_list: list of arguments
+ * Return: 1.
  */
-void print_buffer(char buffer[], int *buff_ind)
+int handle_char(va_list arg_list)
 {
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
+	char arg_c = (char) va_arg(arg_list, int);
 
-*buff_ind = 0;
+	putchar(arg_c);
+	return (1);
+}
+/**
+ * handle_string - function to handle arguments.
+ * @arg_list: list of arguments
+ * Return: a number
+ */
+int handle_string(va_list arg_list)
+{
+	int count = 0;
+	char *arg_s = va_arg(arg_list, char *);
+
+	while (*arg_s)
+	{
+		putchar(*arg_s++);
+		count++;
+	}
+	return (count);
+}
+/**
+ * handle_percent - function that returns a character.
+ * Return: 1
+ */
+int handle_percent(void)
+{
+	putchar('%');
+	return (1);
 }
