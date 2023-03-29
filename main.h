c29234553d7b826c4cf49905b077faf636d80868
valouwase <uwasevalo41@gmail.com>
2023-03-28 18:35:42 -0700

    new changes on task 0

--- main.h
+++ main.h
@@ -9,10 +9,5 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int _position(const char *s, int n);
-int _strlen(char *s);
-int _abs(int n);
-int _numlen(int n);
-char *_strcat(char *dest, char *src, int n);
-void *rev_string(char *s);
+
 #endif /* MAIN_H */
